62c4df9be5689b1a6cb69174d0f7371d04e6b052
MohamedAmr23 <amrm96754@gmail.com>
2024-09-20 14:44:43 +0300

    complete translation to signUp and signIn forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,13 @@
-// import  { useContext } from 'react'
-// import { Link } from 'react-router-dom'
-// import {ShopContext} from '../context/ShopContext.jsx'
 
-// const Login = () => {
-//   const {submit}=useContext(ShopContext)
-//   const formik=useFormik({
-
-//   })
-//   return (
-//     <form onSubmit={submit} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
-//         <div className='flex items-center gap-2'>
-//           <p className='prata-regular text-3xl'>Login</p>
-//           <hr className='w-8 h-[1.5px] bg-gray-800 border-none'/>
-//         </div>
-//         <input type='email' placeholder='Email' className='w-full px-3 py-2 border border-gray-800' required/>
-//         <input type='password' placeholder='Password' className='w-full px-3 py-2 border border-gray-800' required/>
-//         <div className='w-full flex items-center justify-between cursor-pointer text-sm'>
-//             <p>Forgot your password?</p>
-//            <Link to='/sign-up'><p>Create account</p></Link> 
-//         </div>
-//         <button className='text-center bg-black text-white font-light px-8 py-2 mt-4'>Sign In</button>
-//     </form>
-//   )
-// }
-
-// export default Login
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../context/ShopContext.jsx';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-
+import { useTranslation } from 'react-i18next'
 const Login = () => {
   const { submit ,currentMode} = useContext(ShopContext);
+  const {t,i18n}=useTranslation()
   
   const formik = useFormik({
     initialValues: {
@@ -54,11 +29,12 @@ const Login = () => {
 
   return (
     <form 
+      dir={i18n.language==='ar'?'rtl':'ltr'}
       onSubmit={formik.handleSubmit} 
       className={`flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4  ${currentMode==='light'?'text-gray-800':'text-gray-50'}`}
     >
       <div className='flex items-center gap-2'>
-        <p className='prata-regular text-3xl'>Login</p>
+        <p className='prata-regular text-3xl'>{t('Login')}</p>
         <hr className='w-8 h-[1.5px] bg-gray-800 border-none' />
       </div>
 
@@ -66,7 +42,7 @@ const Login = () => {
       <input
         type='email'
         name='email'
-        placeholder='Email'
+        placeholder={i18n.language==='ar'?'الايميل ':'Email'}
         className='w-full px-3 py-2 border border-gray-800'
         value={formik.values.email}
         onChange={formik.handleChange}
@@ -81,7 +57,7 @@ const Login = () => {
       <input
         type='password'
         name='password'
-        placeholder='Password'
+        placeholder={i18n.language==='ar'?'كلمة السر':'password'}
         className='w-full px-3 py-2 border border-gray-800'
         value={formik.values.password}
         onChange={formik.handleChange}
@@ -93,15 +69,15 @@ const Login = () => {
       ) : null}
 
       <div className='w-full flex items-center justify-between cursor-pointer text-sm'>
-       <Link to='/forget-password'><p>Forgot your password?</p></Link> 
-        <Link to='/sign-up'><p>Create account</p></Link>
+       <Link to='/forget-password'><p>{t('Forgot your password?')}</p></Link> 
+        <Link to='/sign-up'><p>{t('Create account')}</p></Link>
       </div>
 
       <button 
         type='submit' 
         className='text-center bg-black text-white font-light px-8 py-2 mt-4'
       >
-        Sign In
+        {t('Sign In')}
       </button>
     </form>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom'
 import {ShopContext} from '../context/ShopContext.jsx'
 import { useFormik } from 'formik'
 import * as Yup from 'yup';
+import { useTranslation } from 'react-i18next'
+
 const SignUp = () => {
   const {submit , currentMode}=useContext(ShopContext)
+  const {t,i18n}=useTranslation()
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -26,17 +29,18 @@ const SignUp = () => {
   });
   return (
     <form
+      dir={i18n.language==='ar'?'rtl':'ltr'}
       onSubmit={formik.handleSubmit}
       className={`flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4  ${currentMode==='light'?'text-gray-800':'text-gray-50'}`}
     >
       <div className="flex items-center gap-2">
-        <p className="prata-regular text-3xl">Sign Up</p>
+        <p className="prata-regular text-3xl">{t('Sign Up')}</p>
         <hr className={`w-8 h-[1.5px] border-none ${currentMode==='light'?'bg-gray-800':'bg-gray-50'}`} />
       </div>
       <input
         type="text"
         name="name"
-        placeholder="name"
+        placeholder={i18n.language==='ar'?'الاسم':'name'}
         className="w-full px-3 py-2 border border-gray-800"
         value={formik.values.name}
         onChange={formik.handleChange}
@@ -48,7 +52,7 @@ const SignUp = () => {
       ) : null}
       <input
         type="email"
-        placeholder="Email"
+        placeholder={i18n.language==='ar'?'الايميل ':'Email'}
         name="email"
         className="w-full px-3 py-2 border border-gray-800"
         value={formik.values.email}
@@ -62,7 +66,7 @@ const SignUp = () => {
       <input
         type="password"
         name="password"
-        placeholder="Password"
+        placeholder={i18n.language==='ar'?'كلمة السر':'password'}
         className="w-full px-3 py-2 border border-gray-800"
         value={formik.values.password}
         onChange={formik.handleChange}
@@ -73,13 +77,13 @@ const SignUp = () => {
         <div className="text-red-500 text-sm">{formik.errors.password}</div>
       ) : null}
       <div className="w-full flex items-center justify-between cursor-pointer text-sm">
-      <Link to='/forget-password'><p>Forgot your password?</p></Link> 
+      <Link to='/forget-password'><p>{t('Forgot your password?')}</p></Link> 
         <Link to="/login">
-          <p>Login here</p>
+          <p>{t('Login here')}</p>
         </Link>
       </div>
       <button className="text-center bg-black text-white font-light px-8 py-2 mt-4">
-        Sign Up
+        {t('Sign Upp')}
       </button>
     </form>
   );
